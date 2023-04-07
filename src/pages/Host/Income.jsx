4c11ee73653ff2e84ceeeb91cf42311d15af4f62
piyushbhawsar <111758968+piyushbhawsar@users.noreply.github.com>
2023-04-07 21:15:37 +0530

    working state, login auth added

--- src/pages/Host/Income.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function Income(){
-    return (
-        <>
-        <h1>This is Income page for host</h1>
-        </>
-    )
-}
--- src/pages/Host/Reviews.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-export default function Reviews(){
-    return (
-        <>
-        <h1>This is Reviews Page for host</h1>
-        </>
-    )
-}
